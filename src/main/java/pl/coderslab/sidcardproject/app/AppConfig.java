15cf8c99385b9102194b0587ac4d19ab4a98d1eb
krzysztoffff <krzycho@pozaprawem.pl>
2018-01-11 10:11:33 +0100

    Create project structure with upload ability

---
+++ src/main/java/pl/coderslab/sidcardproject/app/AppConfig.java
@@ -0,0 +1,43 @@
+package pl.coderslab.sidcardproject.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({"pl.coderslab.sidcardproject.controller", "pl.coderslab.sidcardproject.bean", "pl.coderslab.sidcardproject.entity"
+	, "pl.coderslab.sidcardproject.exception"})
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    //@Bean(name = "multipartResolver")
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    /*@Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver() {
+
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+        commonsMultipartResolver.setMaxUploadSizePerFile(1000); //bytes
+        return commonsMultipartResolver;
+    }*/
+
+}
